3dbe24b8d6f7ee6931f01f8acd90e6da7b044d80
Nhan Phan <nanicude1234@gmail.com>
2026-07-15 20:57:12 +0700

    Add: find-k-closest-elements - submission-1

---
+++ Data Structures & Algorithms/find-k-closest-elements/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+
+    bool compareGap(int a, int b, int x) {
+        if (abs(a - x) < abs(b - x) || (abs(a - x) == abs(b - x) && a < b)) return true;
+        return false; 
+    }
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        int n = arr.size();
+        int l = 0, r = n - 1;
+        while (l < r && r - l + 1 != k) {
+            if (compareGap(arr[l], arr[r], x)) {
+                r--; //nếu khoảng cách từ l tới x < r tới x 
+            }
+            else {
+                l++;
+            }
+        }
+        return vector<int>(arr.begin() + l, arr.begin() + r + 1);
+    }
+};
